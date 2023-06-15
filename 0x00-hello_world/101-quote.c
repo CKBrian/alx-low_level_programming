29837d153da28b61a4a1c7f44c042f253f8576a0
Brian <chumobrian@gmail.com>
2023-06-15 11:16:58 -0700

    advanced tasks on write output system in c and gcc assembly

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - prints "and that piece of art is useful\" - Dora Korpar,
+ *  2015-10-19\n"
+ * Return: 1
+ */
+
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, message, 58);
+	return (1);
+}
