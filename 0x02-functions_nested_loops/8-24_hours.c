831e84bb1ed6890f8621d64a2a925de674eee96b
Brian <chumobrian@gmail.com>
2023-06-20 16:08:26 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,41 @@
  */
 void jack_bauer(void)
 {
-	int hour, min;
+	int h, m;
 
-	for (hour = 0; )
+	for (h = 0; h < 24; h++ )
+	{
+		for (m = 0; m < 60; m++)
+		{
+		if (h < 10 && m < 10)
+		{
+			_putchar ('0');
+			_putchar (h);
+			_putchar (':');
+			_putchar ('0');
+			_putchar (m);
+		}
+		else if (h < 10 && m > 10)
+		{
+		_putchar ('0');
+		_putchar (h);
+		_putchar (':');
+		_putchar (m);
+		}
+		else if (h > 10 && m  < 10)
+		{
+		_putchar (h);
+		_putchar (':');
+		_putchar ('0'); 
+		_putchar (m);
+		}
+		else
+		{
+		_putchar (h);
+		_putchar (':');
+		_putchar (m);
+		}
+		_putchar ('\n');
+		}
+	}
 }
