3cd5d6d01b394eeab6088263eb94a0806193cfd4
Brian <chumobrian@gmail.com>
2023-07-01 12:48:04 +0000

    String lower to uppercase converter program

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
- * string_ toupper - Changes string to upper
- * Return: String
+ * string_toupper - Changes string from lower to upper case
+ * @str: Pointer to string to be converted
+ * Return: Pointer to uppercase string
  */
 char *string_toupper(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
