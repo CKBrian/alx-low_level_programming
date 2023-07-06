cf71ca34fdd5428973922a5a8f8a0ca3a607807d
Brian <chumobrian@gmail.com>
2023-07-06 11:02:45 +0000

    Program to find square root of a number using recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,21 @@
 #include "main.h"
 /**
+ * _sqrt - Returns square root of a number a
+ * @n: Holds number to test for square root digit
+ * @a: Number to be evaluated
+ * Return: Value of n
+ */
+int _sqrt(int n, int a)
+{
+	if (n == 0)
+		return (-1);
+	if (n * n == a)
+		return (n);
+	return (_sqrt(n - 1, a));
+	return (-1);
+
+}
+/**
  * _sqrt_recursion - Returns square root of a number
  * @n: Number to be square rooted
  * Return: Square root of @n
@@ -11,10 +27,8 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
-		return (1);
-	else if (n * n == a)
-		return (n);
-	return ( n * n +(_sqrt_recursion(n - 1)));
+		return (0);
+	return (_sqrt(n, a));
 
 
 }
