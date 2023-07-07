41345f820092e74f94027f1c682d4f83c5a1fd32
Brian <chumobrian@gmail.com>
2023-07-07 12:49:03 +0000

    Program that prints product of program arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints product of arguments it receives
+ * @argc: Argument count
+ * @argv: Array pointer to program arguments
+ * Return: EXIT_SUCCESS if it rums without errors
+ */
+int main(int argc, char *argv[])
+{
+	int i, product;
+
+	if (argc <= 1)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		product = 1;
+		for (i = 1; i < argc; i++)
+		{
+			product *= atoi(argv[i]);
+		}
+		printf("%d\n", product);
+	}
+	exit(EXIT_SUCCESS);
+}
