504133aceaed9c56b9c41c3747fff66bf0bc10fc
Brian <chumobrian@gmail.com>
2023-07-10 14:20:50 +0000

    Program to Create an array and allocate dynamic memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - Creates and initializes an array of chars
+ * @size: Size of array to be assigned dynamically
+ * @c: Value to initialize array with
+ * Return: Pointer to an array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *str;
+
+	if (size == 0)
+	{
+		return ((void *)0);
+	}
+	str = malloc(size + sizeof(char));
+	for (i = 0; i < 98; i++)
+	{
+		*(str + i) = c;
+	}
+	return (str);
+}
