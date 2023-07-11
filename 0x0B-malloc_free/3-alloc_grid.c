dc8ca0f713422f9803295d9bcb2dc4a627a76a35
Brian <chumobrian@gmail.com>
2023-07-11 08:58:20 +0000

    Functio to dynamically allocate memory to a 2D- array and assign values to it

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - Returns a pointer to an 2-d array of integers
+ * @width: width of array
+ * @height: Height of array
+ * Return: Pointer to an array & on failure NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **p;
+
+	if (width <= 0 || height <= 0)
+		return ((void *)0);
+
+	p = malloc(height * sizeof(p));
+	for (j = 0; j < height; j++)
+	{
+		p[j] = malloc(width * sizeof(int));
+	}
+	if (p == NULL)
+		return ((void *)0);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			p[i][j] = 0;
+		}
+	}
+
+	return (p);
+}
