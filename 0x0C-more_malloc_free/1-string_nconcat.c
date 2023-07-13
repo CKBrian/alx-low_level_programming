2727a14bdc86c2d6914b80c70b78dc47301f2694
Brian <chumobrian@gmail.com>
2023-07-13 18:44:49 +0000

    Function tha concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,29 +12,29 @@ char *memory_alloc(char *s1, char *s2, unsigned int n)
 	unsigned int i, k;
 	char *str;
 
-	for (i = 0; s1 != (void *)0 && s1[i] != '\0'; i++)
-	{}
-	for (k = 0; s2 != (void *)0 && s2[k] != '\0'; k++)
-	{}
-	if (s1 == (void *)0)
+	if (s1 == NULL)
 	{
 		i = 0;
 	}
-	if (s2 == (void *)0)
+	if (s2 == NULL)
 	{
 		k = 0;
 	}
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
+	{}
+	for (k = 0; s2 != NULL && s2[k] != '\0'; k++)
+	{}
 
 	if (n < k)
 	{
-		str = malloc((i * sizeof(*s1)) + n + 1);
+		str = malloc((i + n + 1) * sizeof(*str));
 		if (str == NULL)
 			return ((void *)0);
 	}
-	else if (n >= k)
+	else
 	{
 		n = k;
-		str = malloc((i * sizeof(*s1)) + k + 1);
+		str = malloc(sizeof(*str) * (i + n + 1));
 		if (str == NULL)
 			return ((void *)0);
 
@@ -51,36 +51,27 @@ char *memory_alloc(char *s1, char *s2, unsigned int n)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, /*k*/ j = 0;
+	unsigned int i = 0, j = 0;
 	char *str2;
 
 	str2 = memory_alloc(s1, s2, n);
-	if (*s1 != '\0' && s1 != (void *)0)
+	if (str2 == NULL)
+		return ((void *)0);
+	if (s1 != NULL && *s1 != '\0')
 	{
 		for (i = 0; s1[i] != '\0'; i++)
 		{
 			str2[i] = s1[i];
 		}
-		if (*s2 != '\0' && s2 != (void *)0)
-		{
-		for (j = 0; j < n; j++)
-		{
-			str2[i++] = s2[j];
-		}
-		}
-	str2[i] = '\0';
 	}
-	else
+	if (s2 != NULL && *s2 != '\0')
 	{
-		if (*s2 != '\0' && s2 != (void *)0)
-		{
-		for (j = 0; j < n; j++)
-		{
-			str2[j] = s2[j];
-		}
-		}
-	str2[j] = '\0';
-
+	for (j = 0; s2[j] != '\0' && j < n; j++)
+	{
+		str2[i] = s2[j];
+		i++;
 	}
+	}
+	str2[i] = '\0';
 	return (str2);
 }
