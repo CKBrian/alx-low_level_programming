984da2eed76dc2da1ae73ac12ffa9428f81dbec1
Brian <chumobrian@gmail.com>
2023-07-13 19:33:49 +0000

    Function to allocate memory to a pointer and initialize it with NULL character (_calloc) using malloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - Allocates memory for an array using malloc
+ * @nmemb: Number of array elements
+ * @size: Size of array elements in bytes
+ * Return: Pointer to allocated memory else nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		*(ptr + i) = 0;
+	}
+	return (ptr);
+}
