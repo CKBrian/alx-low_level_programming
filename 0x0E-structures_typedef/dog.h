df5c1991cc64db2cead8a7d379f785452da368db
Brian <chumobrian@gmail.com>
2023-07-17 17:36:40 +0000

    created and defined a new structure struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure that stores dog age name and owner details
+ * @name: Pointer to dog name string
+ * @age: Age of dog
+ * @owner: Pointer to dog owner string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/*dog_t *new_dog(char *name, float age, char *owner);*/
+/*void free_dog(dog_t *d);*/
+#endif /* DOG_H */
