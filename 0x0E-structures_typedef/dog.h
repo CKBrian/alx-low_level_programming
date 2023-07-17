8e4616c9c8ddf28d8b64cd779dd5c04165d24bf9
Brian <chumobrian@gmail.com>
2023-07-17 17:52:56 +0000

    created and defined a new structure struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,3 @@
-#ifndef DOG_H
-#define DOG_H
 /**
  * struct dog - Structure that stores dog age name and owner details
  * @name: Pointer to dog name string
@@ -12,4 +10,3 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif /* DOG_H */
