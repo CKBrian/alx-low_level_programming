0051c79f07e3d20b5193cdae8599384e50575321
Brian <chumobrian@gmail.com>
2023-07-19 20:04:08 +0000

    feat: Create program that prints the opecode of its own main function in 100-main_opecodes.c file

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the opscode of its own main function
+ * @argc: Argument count
+ * @argv: Pointer to argument string array
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	unsigned char *adress = __builtin_return_address(0);
+	int i, byte = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (byte <= 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < byte; i++)
+	{
+		printf("%02x ", *adress++);
+	}
+	putchar ('\n');
+	return (0);
+}
