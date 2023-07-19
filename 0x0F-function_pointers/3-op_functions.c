124cf5d791b0c85b36b4a55f7c6f2e4e0d384dda
Brian <chumobrian@gmail.com>
2023-07-19 17:21:09 +0000

    feat: Create a file with add, subtract, division, multiplication & modulo functions for calculator program in 3-op_functios.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - Adds two numbers and returns sum
+ * @a: First number
+ * @b: Second  number
+ * Return: Sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two numbers and returns result
+ * @a: First number
+ * @b: Second  number
+ * Return: Result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Multiplies two numbers and returns the product
+ * @a: First number
+ * @b: Second number
+ * Return: Product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Divides two numbers and returns divident
+ * @a: First number
+ * @b: Second  number
+ * Return: Divident
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Divides two numbers and returns remainder
+ * @a: First number
+ * @b: Second number
+ * Return: Remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
