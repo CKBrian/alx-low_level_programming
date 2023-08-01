8ff8298daf68950e4a28416e9982aab86820719b
Brian <chumobrian@gmail.com>
2023-08-01 14:43:32 +0300

    feat: Create a funtion that reverses a linked list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a listint_t list
+ * @head: pointer to the head node
+ * Return: pointer to reversed list head node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *ptemp = NULL, *ntemp = *head;
+
+	if (head == NULL)
+		return (NULL);
+	while (ntemp->next != NULL)
+	{
+		ntemp = ntemp->next;
+		(*head)->next = ptemp;
+		ptemp = *head;
+		*head = ntemp;
+	}
+	(*head)->next = ptemp;
+
+	return (*head);
+}
