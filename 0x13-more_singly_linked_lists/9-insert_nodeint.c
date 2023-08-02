31bc809b752c5c0a605f46ebef16e75b2d5a4c4c
Brian <chumobrian@gmail.com>
2023-08-02 12:14:02 +0300

    fix: Fix case head == NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,27 +11,27 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp = NULL, *newnode = NULL;
 	unsigned int i;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
-	temp = *head;
-	for (i = 0; temp->next != NULL; i++)
-		temp = temp->next;
-	if (idx > i)
-		return (NULL);
-	temp = *head;
-
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
-
-	if (idx == 0)
+	if (head == NULL)
+	{
+		return (newnode);
+	}
+	else if (idx == 0 || *head == NULL)
 	{
 		newnode->next = *head;
 		*head = newnode;
 	}
 	else
 	{
+		temp = *head;
+		for (i = 0; temp->next != NULL; i++)
+			temp = temp->next;
+		if (idx > i)
+			return (NULL);
+		temp = *head;
 		for (i = 1; i < idx; i++)
 			temp = temp->next;
 		newnode->next = temp->next;
