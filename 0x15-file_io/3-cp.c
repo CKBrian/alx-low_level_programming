0622d379e7ba913c7505e7af0aae379ee1fad256
Brian <chumobrian@gmail.com>
2023-08-08 09:52:17 +0300

    fix:  case: ./3-cp does_not_exist tmp and Check read return value

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 	fdo1 = open(argv[1], O_RDONLY);
 	if (fdo1 < 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(1, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
 	fdo2 = open(argv[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
 	if (fdo2 < 0)
@@ -28,16 +28,16 @@ int main(int argc, char **argv)
 	while (fdr > 0)
 	{
 		fdr = read(fdo1, buffer, 1024);
+		if (fdr < 0)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98); }
 		fdw = write(fdo2, buffer, fdr);
 		if (fdw < 0)
 		{
 			close(fdo1);
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99); } }
-	if (fdr < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98); }
 	if (close(fdo1) < 0)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", (close(fdo1)));
