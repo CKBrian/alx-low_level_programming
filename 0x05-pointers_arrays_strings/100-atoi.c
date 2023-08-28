664678383cdefe40a64ccf4bf2a83b135b513e7b
Brian <chumobrian@gmail.com>
2023-08-28 17:04:06 +0300

    Feat: create a function that converts a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,45 @@
 #include "main.h"
 /**
- * int _atoi - Converts a string to a character
+ * _atoi - Converts a string to a character
  * @s: String to be converted
- * Return: 0 always
+ * Return: number/integer
  */
+
 int _atoi(char *s)
 {
-	int i;
+	int i, num = 0, flag = 1, lbreak = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		if ((s[i] >= '0' && s[i] <= '9') || s[i] == '-' || s[i] == '+')
 		{
-			return ((int)s[i]);
+
+			if (s[i] == '+' && (s[i + 1] >= '0' && s[i + 1] <= '9'))
+				i++;
+			else if (s[i] == '-' && (s[i + 1] >= '0' && s[i + 1] <= '9'))
+				flag = -1;
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				num += s[i] - '0';
+				if (s[i + 1] >= '0' && s[i + 1] <= '9')
+				{
+					num *= 10;
+				}
+				else if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+				{
+					lbreak = 1;
+					i++;
+					break;
+				}
+				i++;
+			}
+
+		}
+		if (lbreak == 1)
+		{
 			break;
 		}
-		else
-			return (0);
 	}
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	if ((s[i] >= '0' && s[i] <= '9') || s[i] == '-' || s[i] == '+')
-	{
-		if (s[i] == '+' && (s[i + 1] >= '0' && s[i + 1] <= '9'))
-			continue;
-		else if (s[i] == '-' && (s[i + 1] >= '0' && s[i + 1] <= '9'))
-		{
-			return ('-');
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-			return ((int)s[i]);
-	}
-	}
-	return (0);
+	num *= flag;
+	return (num);
 }
