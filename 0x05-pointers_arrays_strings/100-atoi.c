00f20b815c20fde3d59b4698c74668e2e5de6127
Brian <chumobrian@gmail.com>
2023-08-28 18:36:34 +0300

    Fix: edge case INT_MIN

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include "main.h"
 /**
  * _atoi - Converts a string to a character
@@ -7,7 +8,7 @@
 
 int _atoi(char *s)
 {
-	int i, num = 0, flag = 1, lbreak = 0;
+	int i, num = 0, flag = 1, lbreak = 0, k = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -16,23 +17,27 @@ int _atoi(char *s)
 
 			if (s[i] == '+' && (s[i + 1] >= '0' && s[i + 1] <= '9'))
 				i++;
-			else if (s[i] == '-' && (s[i + 1] >= '0' && s[i + 1] <= '9'))
-				flag = -1;
+			else if (s[i] == '-')
+			{
+				k++;
+				if (k % 2 != 0)
+					flag = -1;
+				else
+					flag = 1; }
 			while (s[i] >= '0' && s[i] <= '9')
 			{
 				num += s[i] - '0';
 				if (s[i + 1] >= '0' && s[i + 1] <= '9')
 				{
 					num *= 10;
-				}
+					if (num == 2147483640 && s[i + 1] == '8' && flag == -1)
+						return (INT_MIN); }
 				else if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
 				{
 					lbreak = 1;
 					i++;
-					break;
-				}
-				i++;
-			}
+					break; }
+				i++; }
 
 		}
 		if (lbreak == 1)
