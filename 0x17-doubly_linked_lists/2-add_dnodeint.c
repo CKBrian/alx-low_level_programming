47c06a154e6c7e238bedbeb6775b410ede1a77c7
Brian <chumobrian@gmail.com>
2023-09-15 10:45:44 +0300

    fix(core): Add new node to non empty list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,10 +10,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *newnode;
 
 	newnode = malloc(sizeof(dlistint_t));
-	if (head == NULL || newnode == NULL)
+	if (newnode == NULL)
 		return (NULL);
 	newnode->n = n;
 	newnode->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = newnode;
 	newnode->next = *head;
 
 	*head = newnode;
