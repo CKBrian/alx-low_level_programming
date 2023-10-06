729144d99dd640eb554c856a80f1df9b9db28cfd
Brian <chumobrian@gmail.com>
2023-10-06 15:25:54 +0300

    fix: Initialize hash table array to NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,6 +6,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *new_table;
 
 	new_table = malloc(sizeof(hash_table_t));
@@ -14,7 +15,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
+	{
+		free(new_table);
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 
 	return (new_table);
 }
