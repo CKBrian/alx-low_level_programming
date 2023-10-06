671ebc14fcf6e612d042fe07dda088dd1e411324
Brian <chumobrian@gmail.com>
2023-10-06 13:56:25 +0300

    fix(core): Handle case empty hash table in hash_table_print function

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,6 +8,8 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i, flag = 0;
 	char *value = NULL;
 
+	if (ht == NULL)
+		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
