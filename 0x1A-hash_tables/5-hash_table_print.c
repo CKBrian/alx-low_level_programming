41c433550726e98cabb2952fa8575ecc8d942b4c
Brian <chumobrian@gmail.com>
2023-10-06 13:52:39 +0300

    fix(core): Handle extra space in hash_table_print function

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 		else
 		{
 			printf(", ");
-			printf(" '%s': '%s'", ht->array[i]->key, value);
+			printf("'%s': '%s'", ht->array[i]->key, value);
 		}
 	}
 	printf("}\n");
