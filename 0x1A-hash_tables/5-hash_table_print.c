e94fb859351c969d3b5297d94fd024f085db8dd8
Brian <chumobrian@gmail.com>
2023-10-08 00:39:58 +0300

    fix: Handle case print chained nodes

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,19 +7,23 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, flag = 0;
 	char *value = NULL;
+	hash_node_t *temp;
 
 	if (ht == NULL)
 		return;
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		if (!ht->array[i])
-			continue;
-		value = hash_table_get(ht, ht->array[i]->key);
-		if (flag == 1)
-			printf(", ");
-		printf("'%s': '%s'", ht->array[i]->key, value);
-		flag = 1;
+		temp = ht->array[i];
+		while (temp)
+		{
+			value = hash_table_get(ht, temp->key);
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, value);
+			flag = 1;
+			temp = temp->next;
+		}
 	}
 	printf("}\n");
 }
