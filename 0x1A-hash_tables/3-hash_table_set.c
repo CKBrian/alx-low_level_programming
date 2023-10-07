552298c5967e82d9777ca85af70c5dad135284f9
Brian <chumobrian@gmail.com>
2023-10-08 00:20:19 +0300

    fix(core): Handle case: hashtableset(ht, “Holberton”, “is cool”); hashtableset(ht, “Holberton”, “is soooo cooool”); hashtableset(ht, “Holberton”, “is awesome”)

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,8 +35,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	strcpy(temp->key, key);
 	strcpy(temp->value, value);
 
-	if (temp == NULL)
-		temp->next = NULL;
+	temp->next = NULL;
+	if (ht->array[index] && strcmp(key, ht->array[index]->key) == 0)
+		free(ht->array[index]);
 	else
 		temp->next = ht->array[index];
 
