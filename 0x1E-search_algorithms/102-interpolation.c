2edb7f89064325abd5280a30ec4c57a36fa6f5c1
Brian <chumobrian@gmail.com>
2024-01-17 22:53:43 +0300

    feat: Add a function that searches for a value in a sorted array of integers using the interpolation search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,7 +11,7 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0, high = size - 1, pos;
 
-	if (array == NULL)
+	if (array == NULL || value < array[0])
 		return (-1);
 	while (value >= array[low] && value <= array[high])
 	{
