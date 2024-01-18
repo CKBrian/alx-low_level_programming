d3299f9cf0f2b3c65fcefe21ac02dde875c7c09c
Brian <chumobrian@gmail.com>
2024-01-18 21:50:59 +0300

    feat: Add a function that searches for a value in a sorted array of integers using the Jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,5 +1,17 @@
 #include "search_algos.h"
 /**
+ * min - returns the minimum number
+ * @num1: first number
+ * @num2: second number
+ * Return: number
+ */
+size_t min(size_t num1, size_t num2)
+{
+	if (num1 > num2)
+		return (num2);
+	return (num1);
+}
+/**
  * jump_search - searches for a value in a sorted array of integers
  * using the Jump search algorithm
  * @array: pointer to the first element of the array to search in
@@ -9,33 +21,29 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int l_bd, u_bd, i, interval, idx = size - 1;
+	size_t l_bd, u_bd, i, interval, idx = size - 1;
 
-	if (array == NULL || value < array[0])
+	if (array == NULL || size == 0 || value < array[0])
 		return (-1);
 	interval = sqrt(size);
-	for (i = 0; i <= idx || (i > idx && value <= array[idx]); i += interval)
+	for (i = 0; i <= idx || (i > idx /*&& value <= array[idx]*/); i += interval)
 	{
 		if (value == array[0] && i == 0)
 			continue;
-		if ((value <= array[i] && i <= idx) || (i > idx && value <= array[idx]))
+		if ((value <= array[i] && i <= idx) || (i > idx /*&& value >= array[idx]*/))
 		{
 			l_bd = i - interval;
 			u_bd = i;
-			printf("Value found between indexes [%d] and [%d]\n", l_bd, u_bd);
+			printf("Value found between indexes [%ld] and [%ld]\n", l_bd, u_bd);
 			break;
 		}
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
 
-	/* checks if value searched is out of range */
-	if (value > array[idx])
-		return (-1);
-
 	/* linear search algorithm*/
-	for (i = l_bd; i <= u_bd && i <= idx; i++)
+	for (i = l_bd; i <= min(u_bd, idx); i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (value == array[i])
 			return (i);
 	}
